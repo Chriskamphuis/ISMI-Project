eebf8ea457b83aca36f943164657eb0b4753ab7b
Chris Kamphuis <mail@chriskamphuis.com>
2017-06-07 05:10:52 +0200

    fixes

--- scripts/write_submission.py
+++ scripts/write_submission.py
@@ -4,11 +4,14 @@ import numpy as np
 import datetime
 import pandas as pd
 from pytz import timezone
-from scipy.misc import imread, imresize
+import scipy
+from scipy.misc import imread, imresize, imsave
 from network import Network
+from augmenter import chain_augmenters
+
 
 SUBMISSION_DIR = os.path.join('..','data','submissions')
-TEST_DIR = os.path.join('..','data','images','raw','test') #!!SHOULD BE REPRLACED WITH THE PREPOCESSED FOLDER IF THE IMAGES EXIST!!
+TEST_DIR = os.path.join('..','data','images','pre-old','test') #!!SHOULD BE REPRLACED WITH THE PREPOCESSED FOLDER IF THE IMAGES EXIST!!
 CLASSES = ['Type_1','Type_2','Type_3']
 
 '''
@@ -21,22 +24,36 @@ parser.add_argument('--test', help="Directory of the test data", default=TEST_DI
 
 args = parser.parse_args()
 
-output_weights_name = str(args.weights)
-model_name = str(args.model)
+input_weights_name = str(args.weights)
+arch = str(args.model)
 TEST_DIR = str(args.test)
-
+import random
 
 def load_test_data(loadPath):
     '''
-    Load test data
+    Load test data. We apply the same augmentations as in the training
+    process so that we feed the net with what it expects
     '''
     images = []
     image_names = []
     photos = os.listdir(loadPath)
+    size = (224,224,3)
+    augmenter = chain_augmenters(flip=True,
+                                  noise=False,
+                                  smooth=True,
+                                  blur_interval=0.3,
+                                  rotate=False,
+                                  max_angle=5,
+                                  gamma=True,
+                                  zoom=True,
+                                  transform=False)
+    augmenter.randomize()
     for photo in photos:
         if (not 'Thumbs.db' in photo):
             image = imread(os.path.join(loadPath,photo))
-            image = imresize(image,size=(224,224,3))
+            image = imresize(image,size=size)
+            image= augmenter.augment(image)[0]
+            #scipy.misc.imsave('/lustre2/0/ismikag2017/ISMI-project/scripts/delete/'+str(random.randint(0,99999))+'.jpg',image)
             images.append(image)
             image_names.append(photo)
     return (image_names, np.array(images))
@@ -74,9 +91,12 @@ def write_submission_file(image_names, predictions):
 
 
 if __name__ == "__main__":
+    print('loading test images from ',TEST_DIR)
     image_names,images = load_test_data(TEST_DIR)
-    network = Network(model_name,output_weights_name)
+    print('loading network ',input_weights_name)
+    network = Network(arch,input_weights_name)
     network.compile(False)
     predictions = network.predict(images)
+    print('writting submission file')
     image_names, images = sort(image_names, predictions)
     write_submission_file(image_names,predictions)
